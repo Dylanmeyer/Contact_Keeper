83b1387204da9c6e5259750cc3bc49d73a7afd9b
Dylanmeyer <61458670+Dylanmeyer@users.noreply.github.com>
2020-06-22 17:54:16 +0200

    Add files via upload

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
+import contactContext from "../../context/contact/contactContext";
+
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const context = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+  return (
+    <div className="navbar bg-danger">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes,
+};
+
+Navbar.defaultProps = {
+  title: "Kontact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this Application</h1>
+      <p className="my-1">
+        This is a full stack Reacy Application for Keeping contacts
+      </p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+export default About;
